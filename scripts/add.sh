5307d34060eadf692ed97a04c5cf23ecd71439fe
zaky <ahmed6mohamed6@gmail.com>
2023-12-31 13:21:03 +0200

    Add GitHub Actions workflows for adding, subtracting, resetting, and notifying the count of Quran lessons.

---
+++ scripts/add.sh
@@ -0,0 +1,16 @@
+add() {
+
+    local NUM1=$1
+    local NUM2=$2
+
+    SUM=$((NUM1 + NUM2))
+}
+
+IFS=$'\n' read -r -d '' COUNT < count.txt
+
+NUM1=$COUNT
+NUM2=$1
+
+add $NUM1 $NUM2
+
+echo -n "$SUM" > count.txt
